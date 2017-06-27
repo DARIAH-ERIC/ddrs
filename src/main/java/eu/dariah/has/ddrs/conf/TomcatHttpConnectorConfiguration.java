3e8027e1f07885176abb5ea3166cfb696faa325e
yoann <yoann.moranville@gmail.com>
2017-06-27 12:44:44 +0200

    Add port 8080 for tomcat container -> only to redirect to https

---
+++ src/main/java/eu/dariah/has/ddrs/conf/TomcatHttpConnectorConfiguration.java
@@ -0,0 +1,36 @@
+package eu.dariah.has.ddrs.conf;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by yoann on 20.06.17.
+ */
+@Configuration
+public class TomcatHttpConnectorConfiguration {
+
+    @Value("${server.http.port}")
+    private int httpPort;
+
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return new EmbeddedServletContainerCustomizer() {
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+                if (container instanceof TomcatEmbeddedServletContainerFactory) {
+                    TomcatEmbeddedServletContainerFactory containerFactory =
+                            (TomcatEmbeddedServletContainerFactory) container;
+
+                    Connector connector = new Connector(TomcatEmbeddedServletContainerFactory.DEFAULT_PROTOCOL);
+                    connector.setPort(httpPort);
+                    containerFactory.addAdditionalTomcatConnectors(connector);
+                }
+            }
+        };
+    }
+}
