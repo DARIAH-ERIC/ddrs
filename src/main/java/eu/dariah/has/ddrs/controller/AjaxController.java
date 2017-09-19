ed9bb6de728b6bddec499b60a1fd7f68cd765797
yoann <yoann.moranville@gmail.com>
2017-09-19 13:33:16 +0200

    Small refactoring + Review repository and contact pages

--- src/main/java/eu/dariah/has/ddrs/controller/AjaxController.java
+++ src/main/java/eu/dariah/has/ddrs/controller/AjaxController.java
@@ -1,23 +1,12 @@
 package eu.dariah.has.ddrs.controller;
 
-import com.fasterxml.jackson.annotation.JsonView;
 import eu.dariah.has.ddrs.elasticsearch.service.RepositoryService;
-import eu.dariah.has.ddrs.model.AjaxResponseBody;
 import eu.dariah.has.ddrs.model.SearchObject;
-import eu.dariah.has.ddrs.json.JsonViews;
-import eu.dariah.has.ddrs.persistence.dao.IContactRepositoryDAO;
-import eu.dariah.has.ddrs.re3data.search.Re3dataList;
-import eu.dariah.has.ddrs.re3data.search.Repository;
-import eu.dariah.has.ddrs.re3data.details.v2_2.Re3Data;
-import eu.dariah.has.ddrs.service.Re3dataQueryList;
 import eu.dariah.has.ddrs.service.Re3dataRepositoryAPIService;
-import eu.dariah.has.ddrs.service.RepositoryBuilder;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
@@ -32,62 +21,12 @@ public class AjaxController {
     private static final Logger LOGGER = Logger.getLogger(AjaxController.class);
 
     private final Re3dataRepositoryAPIService re3dataRepositoryAPIService;
-    private final IContactRepositoryDAO contactRepositoryDAO;
     private final RepositoryService repositoryService;
 
     @Autowired
-    public AjaxController(Re3dataRepositoryAPIService re3dataRepositoryAPIService, IContactRepositoryDAO contactRepositoryDAO, RepositoryService repositoryService) {
+    public AjaxController(Re3dataRepositoryAPIService re3dataRepositoryAPIService, RepositoryService repositoryService) {
         this.re3dataRepositoryAPIService = re3dataRepositoryAPIService;
-        this.contactRepositoryDAO = contactRepositoryDAO;
         this.repositoryService = repositoryService;
-    }
-
-    @JsonView(JsonViews.Public.class)
-    @RequestMapping(value = "/refreshResultsOld")
-    public AjaxResponseBody getSearchResultViaAjaxOld(@RequestBody SearchObject searchObject, @ModelAttribute("searchObject") SearchObject searchObject1) {
-        searchObject1.setInternSearchParameters(searchObject.getInternSearchParameters());
-        AjaxResponseBody result = new AjaxResponseBody();
-//        Re3dataQueryList re3dataQueryList = new Re3dataQueryList(searchObject);
-//        RestTemplate rest = new RestTemplate();
-
-//        Re3dataList re3dataList = new Re3dataList();
-        long veryStart = System.currentTimeMillis();
-        long start = System.currentTimeMillis();
-        List<eu.dariah.has.ddrs.elasticsearch.model.Repository> repositories = new ArrayList<>();
-        try {
-//            re3dataList = rest.getForObject(re3dataQueryList.getUrl(), Re3dataList.class);
-            repositories = repositoryService.searchWithRestrictions(searchObject, re3dataRepositoryAPIService.getDefaultRepositories(searchObject));
-        } catch (RestClientException e) {
-            LOGGER.error("There was an error in the RestTemplate", e);
-//            re3dataList.setHasError();
-        }
-        LOGGER.debug((System.currentTimeMillis() - start) + "ms");
-
-//        LOGGER.debug("Size of repository list: " + re3dataList.getRepositories().size());
-//        for(Repository repository : re3dataList.getRepositories()) {
-//            start = System.currentTimeMillis();
-//            Re3Data re3Data = re3dataRepositoryAPIService.findRe3Data(repository.getLink().getHref());
-//            LOGGER.debug((System.currentTimeMillis() - start) + "ms for " + repository.getLink().getHref());
-//            if(re3Data.getRepository().size() > 0) {
-//                Re3Data.Repository repo = re3Data.getRepository().get(0);
-//                RepositoryBuilder.addRepositoryDetails(repository, repo, contactRepositoryDAO.findByRepositoryId(repository.getId()));
-//            }
-//        }
-//        LOGGER.info("It took " + (System.currentTimeMillis() - veryStart) + "ms for this search and the retrieval of the detailed data: " + re3dataQueryList.getUrl());
-
-        if(repositories.size() > 0) {
-            result.setCode("200");
-            result.setMsg("");
-            result.setRepositories(repositories);
-//        } else if(re3dataList.getHasError()) {
-//            result.setCode("500");
-//            result.setMsg("Error from Re3data endpoint...");
-        } else {
-            result.setCode("204");
-            result.setMsg("No results...");
-        }
-
-        return result;
     }
 
     @RequestMapping(value = "/refreshResults")
@@ -96,14 +35,14 @@ public class AjaxController {
 
         long start = System.currentTimeMillis();
         List<eu.dariah.has.ddrs.elasticsearch.model.Repository> repositories = new ArrayList<>();
+        ModelAndView modelAndView = new ModelAndView("fragments/results_list :: resultsList");
         try {
             repositories = repositoryService.searchWithRestrictions(searchObject, re3dataRepositoryAPIService.getDefaultRepositories(searchObject));
         } catch (RestClientException e) {
             LOGGER.error("There was an error in the RestTemplate", e);
+            modelAndView.addObject("error", "error");
         }
         LOGGER.debug((System.currentTimeMillis() - start) + "ms");
-
-        ModelAndView modelAndView = new ModelAndView("fragments/results_list :: resultsList");
         modelAndView.addObject("repositories", repositories);
         return modelAndView;
     }
--- src/main/java/eu/dariah/has/ddrs/controller/RepositoryController.java
+++ src/main/java/eu/dariah/has/ddrs/controller/RepositoryController.java
@@ -4,12 +4,6 @@ import eu.dariah.has.ddrs.elasticsearch.model.Repository;
 import eu.dariah.has.ddrs.elasticsearch.service.RepositoryService;
 import eu.dariah.has.ddrs.persistence.dao.IContactRepositoryDAO;
 import eu.dariah.has.ddrs.persistence.model.ContactRepository;
-import eu.dariah.has.ddrs.re3data.details.v2_2.Re3Data;
-import eu.dariah.has.ddrs.re3data.extra.RepositoryDetail;
-import eu.dariah.has.ddrs.re3data.search.Link;
-//import eu.dariah.has.ddrs.re3data.search.Repository;
-import eu.dariah.has.ddrs.service.Re3dataRepositoryAPIService;
-import eu.dariah.has.ddrs.service.RepositoryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,47 +11,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 /**
  * Created by yoann on 12.06.17.
  */
 @Controller
 public class RepositoryController {
-
-    private final Re3dataRepositoryAPIService re3dataRepositoryAPIService;
     private final RepositoryService repositoryService;
     private final IContactRepositoryDAO contactRepositoryDAO;
 
     @Autowired
-    public RepositoryController(Re3dataRepositoryAPIService re3dataRepositoryAPIService, IContactRepositoryDAO contactRepositoryDAO, RepositoryService repositoryService) {
-        this.re3dataRepositoryAPIService = re3dataRepositoryAPIService;
+    public RepositoryController(IContactRepositoryDAO contactRepositoryDAO, RepositoryService repositoryService) {
         this.contactRepositoryDAO = contactRepositoryDAO;
         this.repositoryService = repositoryService;
     }
 
     @RequestMapping(value = "/selectRepository", method = RequestMethod.GET)
     public String viewDetails(@RequestParam(value = "id") String identifier, Model model) {
-        if(identifier.startsWith("re3"))
-            identifier = identifier.replace("re3", "");
+        if(identifier.startsWith("r3d"))
+            identifier = identifier.replace("r3d", "");
         Repository repository = repositoryService.searchByRe3Identifier(identifier);
+        ContactRepository contactRepository = contactRepositoryDAO.findByRepositoryId("r3d" + identifier);
+        if(contactRepository != null)
+            model.addAttribute("contact", contactRepository.getContact());
         model.addAttribute("repository", repository);
         return "repository";
     }
-
-//    @RequestMapping(value = "/selectRepository", method = RequestMethod.POST)
-//    public String viewDetails(@RequestParam(value = "id") String identifier, @RequestParam Map<String, String> allRequestParameters, Model model) {
-//        if(identifier.startsWith("re3"))
-//            identifier = identifier.replace("re3", "");
-//        Repository repository = repositoryService.searchByRe3Identifier(identifier);
-//        model.addAttribute("repository", repository);
-//        allRequestParameters.remove("_csrf");
-//        allRequestParameters.remove("id");
-//        model.addAttribute("allRequestParameters", allRequestParameters);
-//        return "repository";
-//    }
 
     @RequestMapping(value = "/contactForm", method = RequestMethod.GET)
     public String viewContactForm(@RequestParam(value = "id") String identifier, Model model) {
