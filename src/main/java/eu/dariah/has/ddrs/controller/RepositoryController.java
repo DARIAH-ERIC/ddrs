7d18ccb0ab6ca656c192bff57c0e4309be979c47
yoann <yoann.moranville@gmail.com>
2017-11-15 14:16:43 +0100

    Add external conf + add recaptcha

--- src/main/java/eu/dariah/has/ddrs/controller/RepositoryController.java
+++ src/main/java/eu/dariah/has/ddrs/controller/RepositoryController.java
@@ -4,10 +4,18 @@ import eu.dariah.has.ddrs.elasticsearch.model.Repository;
 import eu.dariah.has.ddrs.elasticsearch.service.RepositoryService;
 import eu.dariah.has.ddrs.persistence.dao.IContactRepositoryDAO;
 import eu.dariah.has.ddrs.persistence.model.ContactRepository;
+import eu.dariah.has.ddrs.service.RecaptchaService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by yoann on 12.06.17.
@@ -16,11 +24,16 @@ import org.springframework.web.bind.annotation.*;
 public class RepositoryController {
     private final RepositoryService repositoryService;
     private final IContactRepositoryDAO contactRepositoryDAO;
+    private final RecaptchaService captchaService;
+
+    @Value("${recaptcha.site}")
+    private String recaptchaSite;
 
     @Autowired
-    public RepositoryController(IContactRepositoryDAO contactRepositoryDAO, RepositoryService repositoryService) {
+    public RepositoryController(IContactRepositoryDAO contactRepositoryDAO, RepositoryService repositoryService, RecaptchaService captchaService) {
         this.contactRepositoryDAO = contactRepositoryDAO;
         this.repositoryService = repositoryService;
+        this.captchaService = captchaService;
     }
 
     @GetMapping(value = "/selectRepository")
@@ -41,12 +54,25 @@ public class RepositoryController {
             identifier = identifier.replace("r3d", "");
         Repository repository = repositoryService.searchByRe3Identifier(identifier);
         model.addAttribute("repository", repository);
+        model.addAttribute("recaptchaSite", recaptchaSite);
         return "contact_form";
     }
 
     @PostMapping(value = "/sendResults")
-    public String sendResults(Model model) {
+    public String sendResults(Model model, @RequestParam(value = "id") String identifier, @RequestParam(name="g-recaptcha-response") String recaptchaResponse, HttpServletRequest request) {
+        String ip = request.getRemoteAddr();
+        String captchaVerifyMessage =
+                captchaService.verifyRecaptcha(ip, recaptchaResponse);
+
+        if (StringUtils.isNotEmpty(captchaVerifyMessage)) {
+            if(identifier.startsWith("r3d"))
+                identifier = identifier.replace("r3d", "");
+            Repository repository = repositoryService.searchByRe3Identifier(identifier);
+            model.addAttribute("repository", repository);
+            model.addAttribute("recaptchaSite", recaptchaSite);
+            model.addAttribute("error", captchaVerifyMessage);
+            return "contact_form";
+        }
         return "results_sent";
     }
-
 }
---
+++ src/main/java/eu/dariah/has/ddrs/service/RecaptchaService.java
@@ -0,0 +1,58 @@
+package eu.dariah.has.ddrs.service;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * From: https://github.com/sanaulla123/samples/blob/master/recaptcha-demo/src/main/java/info/sanaulla/recaptchademo/RecaptchaService.java
+ */
+
+@Service
+public class RecaptchaService {
+
+    private static final Logger LOG = Logger.getLogger(RecaptchaService.class);
+
+    @Value("${recaptcha.secret}")
+    private String recaptchaSecret;
+
+    private static final String GOOGLE_RECAPTCHA_VERIFY_URL = "https://www.google.com/recaptcha/api/siteverify";
+
+    private final RestTemplateBuilder restTemplateBuilder;
+
+    @Autowired
+    public RecaptchaService(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplateBuilder = restTemplateBuilder;
+    }
+
+    public String verifyRecaptcha(String ip, String recaptchaResponse){
+        Map<String, String> body = new HashMap<>();
+        body.put("secret", recaptchaSecret);
+        body.put("response", recaptchaResponse);
+        body.put("remoteip", ip);
+        LOG.debug("Request body for recaptcha: " + body.toString());
+        ResponseEntity<Map> recaptchaResponseEntity =
+                restTemplateBuilder.build()
+                        .postForEntity(GOOGLE_RECAPTCHA_VERIFY_URL+
+                                        "?secret={secret}&response={response}&remoteip={remoteip}",
+                                body, Map.class, body);
+        LOG.debug("Response from recaptcha: " + recaptchaResponseEntity.toString());
+        Map<String, Object> responseBody = recaptchaResponseEntity.getBody();
+        boolean recaptchaSucess = (Boolean)responseBody.get("success");
+        if (!recaptchaSucess) {
+            List<String> errorCodes = (List)responseBody.get("error-codes");
+            return errorCodes.stream().collect(Collectors.joining(", "));
+        } else {
+            return StringUtils.EMPTY;
+        }
+    }
+}
